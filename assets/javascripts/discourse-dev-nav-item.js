285855f80e427a90205217e4aa1019516910e7a7
Robin Ward <robin.ward@gmail.com>
2014-02-27 15:40:53 -0500

    Pluralize Developer

--- assets/javascripts/discourse-dev-nav-item.js
+++ assets/javascripts/discourse-dev-nav-item.js
@@ -1,4 +1,4 @@
 (function() {
-  Discourse.HTML.setCustomHTML('extraNavItem', "<li><a href='/groups/developer'>Dev Tracker</a></li>");
+  Discourse.HTML.setCustomHTML('extraNavItem', "<li><a href='/groups/developers'>Dev Tracker</a></li>");
 })();
 
